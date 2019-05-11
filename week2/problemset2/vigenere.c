b00b867fdc00929c15e52ab3e62179850f7c933f
Melissa <melissapotter.student@careerdevs.com>
2019-05-11 21:06:26 +0000

    vigenere

---
+++ week2/problemset2/vigenere.c
@@ -0,0 +1,65 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+int shift(char c);
+int main(int argc, string argv[])
+{
+    if (argc >= 1) //if an input is entered after command line
+        for (int i = 1; i < argc; i++)
+        {
+            for (int j = 0, n = strlen(argv[i]); j < n; j++)
+            {
+                if (isalpha(argv[i][j]) == false)
+                {
+                    printf("Usage: %s key\n", argv[0]);
+                    return 1;
+                }
+            }
+            string plaintext = get_string("plaintext: ");
+            char ciphertext;
+            printf("ciphertext: ");
+            if (strlen(plaintext) > 0)
+//if an input is entered after command line
+                for (int c = 0; c < strlen(plaintext); c++)
+
+// for every word, keeping it less than the number of input letters .. iterate over each letter
+                {
+                    if (isupper(plaintext[c]))
+                    {
+                        ciphertext = ((((plaintext[c] + (shift(argv[1][c])) - 65) % 26) + 65));
+                    }
+                    if (islower(plaintext[c]))
+                    {
+                        ciphertext = ((((plaintext[c] + (shift(argv[1][c])) - 97) % 26) + 97));
+                    }
+                    else if (isalpha(plaintext[c]) == false)
+                    {
+                        ciphertext = (plaintext[c]);
+                    }
+                    printf("%c", ciphertext);
+
+                }
+            printf("\n");
+        }
+    else
+    {
+        printf("Usage: %s key\n", argv[0]);
+        return 1;
+    }
+}
+
+int shift(char c)
+//taking input char c and outputting how many numbers to shift by
+{
+    if isupper(c)
+    {
+        int less = c - 65;
+        return less;
+    }
+    else
+    {
+        int smaller = c - 97;
+        return smaller;
+    }
+}

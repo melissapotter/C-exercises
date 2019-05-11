5e9c75c1a6eaa6ef1adb431f7db56bc9ea5c651d
Melissa <melissapotter.student@careerdevs.com>
2019-05-11 17:19:21 +0000

    updated caesar

--- week2/problemset2/caesar.c
+++ week2/problemset2/caesar.c
@@ -6,7 +6,7 @@
 int main(int argc, string argv[])
 
 {
-    if (argc == 2) //if an input is entered after command line
+    if (argc > 1) //if an input is entered after command line
         for (int i = 1; i < argc; i++)
 // for every word while skipping the first word, keeping it less than 2 words .. iterate over each word
         {
@@ -19,28 +19,22 @@ int main(int argc, string argv[])
                     return 1;
                 }
             }
-            int converted = atoi(argv[1]);
+            int key = atoi(argv[1]);
             string plaintext = get_string("plaintext: ");
             printf("ciphertext: ");
-            if (strlen(plaintext) > 1)
+            if (strlen(plaintext) > 0)
 //if an input is entered after command line
                 for (int c = 0; c < strlen(plaintext); c++)
 // for every word, keeping it less than the number of input words .. iterate over each word
                 {
-                    int text = (plaintext[c]);
-                    char cipher = text + converted;
-                    if ((cipher > 65 && cipher < 91) || (cipher > 97 && cipher < 123))
-// if code fits in the ascii table
+                    char text = (plaintext[c]);
+                    if (isupper(text))
                     {
-                        printf("%c", cipher);
+                        printf("%c", ((((plaintext[c] + key) - 65) % 26) + 65));
                     }
-                    else if (isupper(text))
+                    else if (islower(text))
                     {
-                        printf("%c", (((cipher - 65) % 26) + 65));
-                    }
-                    else
-                    {
-                        printf("%c", (((cipher - 97) % 26) + 97));
+                        printf("%c", ((((plaintext[c] + key) - 97) % 26) + 97));
                     }
 
                 }
